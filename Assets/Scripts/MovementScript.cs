1379c8ba41854573e312f7bd163f3607c67cbb79
VJArt <vjartbusiness@gmail.com>
2026-03-26 01:22:43 +0100

    Deadline 0.0.1.3
    Complete revamp of the movement system (camera system still the same). I am taking it more slowly this time and trying to understand what I'm implementing.
    
    Features:
    FPS Camera movement
    Walking (not speed-capped)
    Running (not speed-capped)
    
    Plan for next sprint is adding a variable speed cap system, and a master speed cap, as well as fine tuning the exact speed variables. If time allows it maybe even more features.

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -1,14 +1,20 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MovementScript : MonoBehaviour
 {
+    [Header("Referances")]
+    public Transform orientor;
+    [Header("Movement")]
+    public float speed; // regular speed
+    public float sprSpeed; // speed while sprinting
 
-    [Header("References")]
     private Rigidbody rb;
-    public Transform orientation;
-
+    private Vector2 moveInput;
+    private Vector3 moveDirection;
+    private bool isSprinting;
     
-
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
@@ -18,8 +24,37 @@ public class MovementScript : MonoBehaviour
         
     }
 
+    void Update() 
+    {
+        Debug.Log(moveInput);
+        Debug.Log(isSprinting);
+    }
+
     void FixedUpdate()
     {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        moveDirection = orientor.forward * moveInput.y + orientor.right * moveInput.x; //For Future not, transform.forward represents the blue arrow, and transform.right represents the red arrow. The y axis or the green arrow is transform.up and we aren't touching that now.
+       
+        if (moveDirection.sqrMagnitude > 1f)
+        {
+            moveDirection.Normalize();
+        }
         
+        float currentSpeed = isSprinting ? sprSpeed : speed;
+
+        rb.AddForce(moveDirection * currentSpeed, ForceMode.VelocityChange); 
+    }
+
+    public void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+    public void OnSprint(InputValue value)
+    {
+        isSprinting = value.isPressed;
     }
 }
